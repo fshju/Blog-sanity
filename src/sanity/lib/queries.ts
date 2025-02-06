7bb2b08fd90f703f4b1e7b94d11decf6d75fd9dd
Shahrukh Ishtiaq <159270984+fshju@users.noreply.github.com>
2025-02-06 19:31:22 +0500

    Delete src/sanity/lib/queries.ts

--- src/sanity/lib/queries.ts
+++
@@ -1,8 +0,0 @@
-import { defineQuery } from "next-sanity";
-
-export const blogquery = defineQuery(`*[_type == "blog"] | order(_createdAt asc){
-      "image_url":image.asset->url,
-      title,
-      description,
-      "slug":slug.current
-    }`)
